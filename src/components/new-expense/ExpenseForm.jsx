9722006f74a667e3e456850db8b83a0182a94c55
Bismillah <cupertino@Matraims-MacBook-Pro.local>
2023-12-15 18:33:31 +0600

    fixed conflict

--- src/components/new-expense/ExpenseForm.jsx
+++ src/components/new-expense/ExpenseForm.jsx
@@ -1,89 +1,83 @@
-import React, { useState } from 'react'
-import FormInput from '../UI/input/FormInput'
-import Button from '../UI/button/Button'
-import './ExpenseForm.css'
+import React, { useState } from 'react';
+import FormInput from '../UI/input/FormInput';
+import Button from '../UI/button/Button';
+import './ExpenseForm.css';
 
 const ExpenseForm = ({ onCloseForm, onAddNewExpense }) => {
-	const [title, setTitle] = useState('')
-	const [amount, setAmount] = useState('')
-	const [date, setDate] = useState('')
+  const [title, setTitle] = useState('');
+  const [amount, setAmount] = useState('');
+  const [date, setDate] = useState('');
 
-	const isFormValid =
-		title.trim().length !== 0 &&
-		amount.trim().length !== 0 &&
-		date.trim().length !== 0
+  const isFormValid =
+    title.trim().length !== 0 &&
+    amount.trim().length !== 0 &&
+    date.trim().length !== 0;
 
-	const titleChangeHandler = (e) => {
-		setTitle(e.target.value)
-	}
+  const titleChangeHandler = (e) => {
+    setTitle(e.target.value);
+  };
 
-	const amountChangeHandler = (e) => {
-		setAmount(e.target.value)
-	}
+  const amountChangeHandler = (e) => {
+    setAmount(e.target.value);
+  };
 
-	const dateChangeHandler = (e) => {
-		setDate(e.target.value)
-	}
+  const dateChangeHandler = (e) => {
+    setDate(e.target.value);
+  };
 
-	// const titleRef = useRef()
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (isFormValid) {
+      const expense = {
+        date: new Date(date),
+        title,
+        amount: Number(amount),
+        id: Date.now().toString(),
+      };
+      onAddNewExpense(expense);
 
-	const submitHandler = (e) => {
-		e.preventDefault()
-		if (isFormValid) {
-			const expense = {
-				date: new Date(date),
-				title,
-				amount: Number(amount),
-				id: Date.now().toString(),
-			}
-			onAddNewExpense(expense)
+      setTitle('');
+      setAmount('');
+      setDate('');
+      onCloseForm();
+    } else {
+      alert('Заполните все поля!');
+    }
+  };
 
-			setTitle('')
-			setAmount('')
-			setDate('')
-			onCloseForm()
-			// state batching => пакетирование setState
-		} else {
-			alert('Заполните все поля!')
-		}
-	}
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <FormInput
+          label={'Name'}
+          type="text"
+          onChange={titleChangeHandler}
+          value={title}
+        />
+        <FormInput
+          label={'Price'}
+          inputType="number"
+          value={amount}
+          onChange={amountChangeHandler}
+        />
+        <FormInput
+          label={'Date'}
+          inputType="date"
+          onChange={dateChangeHandler}
+          value={date}
+        />
+      </div>
+      <div className="new-expense__actions">
+        <Button type="button" onClick={onCloseForm}>
+          Cancel
+        </Button>
+        <Button type="submit">Add Expenses</Button>
+      </div>
+    </form>
+  );
+};
 
-	return (
-		<form onSubmit={submitHandler}>
-			<div className='new-expense__controls'>
-				<FormInput
-					label={'Name'}
-					type='text'
-					onChange={titleChangeHandler}
-					value={title}
-				/>
-				<FormInput
-					label={'Price'}
-					inputType='number'
-					value={amount}
-					onChange={amountChangeHandler}
-				/>
-				<FormInput
-					label={'Date'}
-					inputType='date'
-					onChange={dateChangeHandler}
-					value={date}
-				/>
-			</div>
-			<div className='new-expense__actions'>
-				<Button type='button' onClick={onCloseForm}>
-				Cancel
-				</Button>
-				<Button type='submit'>Add Expenses</Button>
-			</div>
-		</form>
-	)
-}
-
-export default ExpenseForm
-
-
-
+export default ExpenseForm;
 
 // git init
 // git add .
